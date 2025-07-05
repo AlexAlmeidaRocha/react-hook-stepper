cf778e082d7fa39fd3e78cd8788bc23ab30bd4cf
alex <alexalmeidarocha@outlook.com.br>
2025-07-05 00:50:26 -0300

    refactor: update build script in package.json and add a new line in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { useStepper } from './useStepper';
 export { StepsProvider } from './StepsContext';
 export { StepsWithProvider } from './StepsWithProvider';
+
 export type {
   StepperConfig,
   StepperContext,
